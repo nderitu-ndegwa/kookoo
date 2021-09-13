8a44ae464c4454ab46bf47cef1739502572c53f5
nderitu-ndegwa <62438015+nderitu-ndegwa@users.noreply.github.com>
2021-09-13 08:09:27 +0300

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -17,16 +17,16 @@ def explore(df):
     st.write(df)
 
     #numerical summary
-    df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
-    numerical_cols = df_types[~df_types['Data Type'].isin(['object', 'bool'])].index.values
+    #df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
+    #numerical_cols = df_types[~df_types['Data Type'].isin(['object', 'bool'])].index.values
 
-    df_types['Count'] = df.count()
-    df_types['Unique Values'] = df.nunique()
-    df_types['Min'] = df[numerical_cols].min()
-    df_types['Max'] = df[numerical_cols].max()
-    df_types['Average'] = df[numerical_cols].mean()
-    df_types['Median'] = df[numerical_cols].median()
-    df_types['St. Dev.'] = df[numerical_cols].std()  
+    #df_types['Count'] = df.count()
+    #df_types['Unique Values'] = df.nunique()
+    #df_types['Min'] = df[numerical_cols].min()
+    #df_types['Max'] = df[numerical_cols].max()
+    #df_types['Average'] = df[numerical_cols].mean()
+    #df_types['Median'] = df[numerical_cols].median()
+    #df_types['St. Dev.'] = df[numerical_cols].std()  
     
     #st.write('Summary:')
     #st.write(df_types)
