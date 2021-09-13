28d0bf7c68a2a5c6ed04dab3f1c85818b87b0588
nderitu-ndegwa <62438015+nderitu-ndegwa@users.noreply.github.com>
2021-09-13 08:11:48 +0300

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -17,16 +17,16 @@ def explore(df):
     st.write(df)
 
     #numerical summary
-    #df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
-    #numerical_cols = df_types[~df_types['Data Type'].isin(['object', 'bool'])].index.values
+    df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
+    numerical_cols = df_types[~df_types['Data Type'].isin(['object', 'bool'])].index.values
 
-    #df_types['Count'] = df.count()
-    #df_types['Unique Values'] = df.nunique()
-    #df_types['Min'] = df[numerical_cols].min()
-    #df_types['Max'] = df[numerical_cols].max()
-    #df_types['Average'] = df[numerical_cols].mean()
-    #df_types['Median'] = df[numerical_cols].median()
-    #df_types['St. Dev.'] = df[numerical_cols].std()  
+    df_types['Count'] = df.count()
+    df_types['Unique Values'] = df.nunique()
+    df_types['Min'] = df[numerical_cols].min()
+    df_types['Max'] = df[numerical_cols].max()
+    df_types['Average'] = df[numerical_cols].mean()
+    df_types['Median'] = df[numerical_cols].median()
+    df_types['St. Dev.'] = df[numerical_cols].std()  
     
     #st.write('Summary:')
     #st.write(df_types)
@@ -35,18 +35,18 @@ def explore(df):
     #Using the pandas profiler(Profile Report)
     pr = ProfileReport(df, explorative=True)
     st_profile_report(pr)
-
+st.cache(ttl=60)
 def get_df(file):
     time.sleep(2)
     # get extension and read file
     extension = file.name.split('.')[1]
 
     if extension.upper() == 'CSV':
-        df = st.cache(pd.read_csv(file))
+        df = pd.read_csv(file)
     elif extension.upper() == 'XLSX':
-        df = st.cache(pd.read_excel(file, engine='openpyxl'))
+        df = pd.read_excel(file, engine='openpyxl')
     elif extension.upper() == 'PICKLE':
-        df = st.cache(pd.read_pickle(file))  
+        df = pd.read_pickle(file)
     
     return df
 
