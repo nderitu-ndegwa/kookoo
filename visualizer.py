1fd8fc437c9b812fef29fbba8bc1e08f021d22d3
nderitu-ndegwa <62438015+nderitu-ndegwa@users.noreply.github.com>
2021-09-15 07:32:56 +0300

    lo and behold!

--- visualizer.py
+++ visualizer.py
@@ -1,28 +1,66 @@
-import pandas as pd
-import streamlit as st
+import pandas as pd 
+import streamlit as st 
+import matplotlib.pyplot as plt 
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
-st.set_page_config(layout='wide')
-st.title("Credit Card Fraud Detection & Visualization")
 
-@st.cache(allow_output_mutation=True)
-def load_data(file):
-    df = pd.read_csv(file)
+st.set_page_config(layout="wide")
+st.title("Credit Card Fraud Detection and visaulization")
+
+#Exploration function(numerical summary)
+def explore(df):
+    st.write('data')
+    st.write(df)
+
+    #numerical summary
+    df_types = pd.DataFrame(df.dtypes, columns=['Data Type'])
+    numerical_cols = df_types[~df_types['Data Type'].isin(['object', 'bool'])].index.values
+
+    df_types['Count'] = df.count()
+    df_types['Unique Values'] = df.nunique()
+    df_types['Min'] = df[numerical_cols].min()
+    df_types['Max'] = df[numerical_cols].max()
+    df_types['Average'] = df[numerical_cols].mean()
+    df_types['Median'] = df[numerical_cols].median()
+    df_types['St. Dev.'] = df[numerical_cols].std()  
+    
+    #st.write('Summary:')
+    #st.write(df_types)
+    #st.line_chart(df)
+
+    #Using the pandas profiler(Profile Report)
+    pr = ProfileReport(df, explorative=True)
+    st_profile_report(pr)
+
+@st.cache
+def get_df(file):
+
+    # get extension and read file
+    extension = file.name.split('.')[1]
+
+    if extension.upper() == 'CSV':
+        df = pd.read_csv(file)
+    elif extension.upper() == 'XLSX':
+        df = pd.read_excel(file, engine='openpyxl')
+    elif extension.upper() == 'PICKLE':
+        df = pd.read_pickle(file)  
+    
     return df
 
-st.sidebar.title("Upload data")    
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+#st.title("Credit Card Fraud Detection and visualization")
+file = st.file_uploader("Upload dataset in .csv or .xlsx format", type=['csv', 'xlsx'], key = "1")
+st.write(file)
 
 option = st.selectbox(
-    'Choose pandas profiling mode:',
-    ('Minimal', 'Explorative', 'Default'))
+'Choose pandas profiling mode:',
+('Minimal', 'Explorative', 'Default'))
 
-if uploaded_file is None:
+if file is None:
     st.write("Please upload your data from the left panel")
 
-if uploaded_file is not None:
-    df = load_data(uploaded_file)
+if file is not None:
+    df = get_df(file)
     st.write('data')
     st.write(df)
     if option == "Default":
@@ -42,3 +80,4 @@ if uploaded_file is not None:
 
     st.title("Statistical Profile Report")
     st_profile_report(pr)
+
