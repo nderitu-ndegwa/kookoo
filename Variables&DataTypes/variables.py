3c7caadb370c024c338e0c1f7c1b5f6b1df2b01c
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-11-01 07:57:03 +0300

    Variables and data types

---
+++ Variables&DataTypes/variables.py
@@ -0,0 +1,35 @@
+# Integer variable
+age = 25
+print(age)  # Output: 25
+
+# Float variable
+price = 9.99
+print(price)  # Output: 9.99
+
+# String variable
+name = "John"
+print(name)  # Output: John
+
+# Boolean variable
+is_student = True
+print(is_student)  # Output: True
+
+# List variable
+fruits = ["apple", "banana", "cherry"]
+print(fruits)  # Output: ['apple', 'banana', 'cherry']
+
+# Tuple variable
+coordinates = (10, 20)
+print(coordinates)  # Output: (10, 20)
+
+# Dictionary variable
+person = {"name": "John", "age": 25, "is_student": True}
+print(person)  # Output: {'name': 'John', 'age': 25, 'is_student': True}
+
+# Set variable
+numbers = {1, 2, 3, 4, 5}
+print(numbers)  # Output: {1, 2, 3, 4, 5}
+
+# Complex variable
+complex_num = 2 + 3j
+print(complex_num)  # Output: (2+3j)
