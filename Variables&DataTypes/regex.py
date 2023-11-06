7f3cd373878f2beda628c08f77f1ac159f0c47ec
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-11-06 04:40:20 +0300

    re.search()

--- Variables&DataTypes/regex.py
+++ Variables&DataTypes/regex.py
@@ -1,5 +1,14 @@
+"""
+#Using the re.match function example
 import re
 line = "Cats are smarter than dogs"
 matchObj = re.match(r'cats', line, re.M|re.I)
+print(matchObj.start(), matchObj.end())
+print("matchObj.group() : ", matchObj.group())
+"""
+#Using the re.search function example
+import re
+line = "Cats are smarter than dogs"
+matchObj = re.search(r'dogs', line, re.M|re.I)
 print(matchObj.start(), matchObj.end())
 print("matchObj.group() : ", matchObj.group())
