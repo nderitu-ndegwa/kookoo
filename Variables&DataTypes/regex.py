2fe54bcd6dbb184c6abda4d4a027f0e86c0f87a7
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-11-06 04:48:41 +0300

    re.sub()

--- Variables&DataTypes/regex.py
+++ Variables&DataTypes/regex.py
@@ -12,10 +12,17 @@ line = "Cats are smarter than dogs"
 matchObj = re.search(r'dogs', line, re.M|re.I)
 print(matchObj.start(), matchObj.end())
 print("matchObj.group() : ", matchObj.group())
-"""
+
 
 #Using re.findall function example
 import re
 string = "Python is fun, but it is also cool"
-obj = re.findall(r'is', string)
+obj = re.findall(r'\w*', string)
+print(obj)
+"""
+
+#Using the re.sub function example
+import re
+string = "Python is fun, but it is also cool"
+obj = re.sub(r'Python', 'Java', string)
 print(obj)
