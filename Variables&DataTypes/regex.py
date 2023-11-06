2892204559b08bd19f5b8e36a5da4ce1a44d60d6
nderitu-ndegwa <nderitundegwa10@gmail.com>
2023-11-06 04:44:46 +0300

    re.findall()

--- Variables&DataTypes/regex.py
+++ Variables&DataTypes/regex.py
@@ -5,10 +5,17 @@ line = "Cats are smarter than dogs"
 matchObj = re.match(r'cats', line, re.M|re.I)
 print(matchObj.start(), matchObj.end())
 print("matchObj.group() : ", matchObj.group())
-"""
+
 #Using the re.search function example
 import re
 line = "Cats are smarter than dogs"
 matchObj = re.search(r'dogs', line, re.M|re.I)
 print(matchObj.start(), matchObj.end())
 print("matchObj.group() : ", matchObj.group())
+"""
+
+#Using re.findall function example
+import re
+string = "Python is fun, but it is also cool"
+obj = re.findall(r'is', string)
+print(obj)
